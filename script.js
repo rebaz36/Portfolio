e743ab7e899d75b0de4fec4c37408d9ee7b3d7ae
rebaz36 <rebaz415@gmail.com>
2022-07-19 05:18:34 +0300

    finalized project

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// Get the modal
-var modal = document.getElementById("mobile-nav");
+const modal = document.getElementById('mobile-nav');
 
-// Get the button that opens the modal
-var btn = document.getElementById("mobileMenu");
+const btn = document.getElementById('mobileMenu');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = function block() {
+  modal.style.display = 'block';
+};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function none() {
+  modal.style.display = 'none';
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = function windownone(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
