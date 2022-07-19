2b827aad5c84fb3c68f07e986fe1c1b192b50a4b
rebaz36 <rebaz415@gmail.com>
2022-07-19 08:37:07 +0300

    Fixed overflowing issue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const modal = document.getElementById('mobile-nav');
+const body = document.getElementById('body');
 
 const btn = document.getElementById('mobileMenu');
 
@@ -6,10 +7,12 @@ const span = document.getElementsByClassName('close')[0];
 
 btn.onclick = function block() {
   modal.style.display = 'block';
+  body.style.overflow = 'hidden';
 };
 
 span.onclick = function none() {
   modal.style.display = 'none';
+  body.style.overflow = 'visible';
 };
 
 window.onclick = function windownone(event) {
